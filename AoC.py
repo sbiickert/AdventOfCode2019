2a107bb7b8a16fcfe250aeff004ad113ac944d78
Simon Biickert <simon@biickert.ca>
2020-12-29 12:51:23 -0700

    Day 1 complete, parts 1 and 2.

---
+++ AoC.py
@@ -0,0 +1,23 @@
+def read_input_file(name, remove_empty_lines):
+	with open(name, 'r') as file:
+		content = file.read()
+		input = content.splitlines()
+	if remove_empty_lines:
+		input = list(filter(lambda line: (line != ''), input))
+	return input
+
+def read_grouped_input_file(name):
+	input = read_input_file(name, False)
+	group = []
+	groups = []
+	for line in input:
+		if line != '':
+			group.append(line)
+		else:
+			groups.append(group)
+			group = []
+	groups.append(group)
+	return groups
+	
+if __name__ == "__main__":
+	print("AoC is a library.")
---
+++ Day01.py
@@ -0,0 +1,22 @@
+import math
+import AoC
+
+def calc_fuel(mass):
+	fuel = 0
+	while mass > 0:
+		mass = math.floor(mass / 3) - 2
+		#print(f'{fuel} + {mass}')
+		if mass > 0:
+			fuel += mass
+	return fuel
+
+def main():
+	input = AoC.read_input_file("Day01_Input.txt", True)
+	total_fuel = 0
+	for mass in input:
+		total_fuel += calc_fuel(int(mass))
+	print(f'The total fuel needed is {total_fuel}.')
+
+if __name__ == "__main__":
+	main()
+	#calc_fuel(100756)
