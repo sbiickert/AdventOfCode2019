e48ae344192ec64fef9d9da6b750519f1f2b70b6
Simon Biickert <simon@biickert.ca>
2020-12-29 16:08:03 -0700

    Day 2 part 2 complete.

--- Day02.py
+++ Day02.py
@@ -1,5 +1,7 @@
 import AoC
 
+TARGET_SOLUTION = 19690720
+
 def cycle_computer(ptr, program):
 	# Read opcode at ptr
 	opcode = program[ptr]
@@ -25,21 +27,34 @@ def cycle_computer(ptr, program):
 
 def main():
 	input = AoC.read_input_file("Day02_Input.txt", True)
-	program = list(map(lambda x: (int(x)), input[5].split(',')))
+	master_program = list(map(lambda x: (int(x)), input[5].split(',')))
 	
-	# Instructions were to replace the first two values with 12 and 2
-	program[1] = 12
-	program[2] = 2
+	solution = 0
+	while solution != TARGET_SOLUTION:
+		for noun in range(0, 100):
+			for verb in range(0, 100):
+				program = master_program.copy()
+				# Part 1 instructions were to replace the first two values with 12 and 2
+				program[1] = noun
+				program[2] = verb
+				
+				last_opcode = 1
+				ptr = 0
+				#print(program)
+				while last_opcode in [1, 2]:
+					(ptr, last_opcode) = cycle_computer(ptr, program)
+					#print(f'ptr: {ptr} last_opcode: {last_opcode}')
+					#print(program)
+				#print(f'End state: {program}')
+				solution = program[0]
+				print(f'The value at address [0] is {solution}')
+				if solution == TARGET_SOLUTION:
+					break
+			if solution == TARGET_SOLUTION:
+				break
 	
-	last_opcode = 1
-	ptr = 0
-	#print(program)
-	while last_opcode in [1, 2]:
-		(ptr, last_opcode) = cycle_computer(ptr, program)
-		#print(f'ptr: {ptr} last_opcode: {last_opcode}')
-		#print(program)
-	#print(f'End state: {program}')
-	print(f'The value at address [0] is {program[0]}')
+	print(f'100 * {noun} + {verb} = {100 * noun + verb}')
+		
 
 if __name__ == "__main__":
 	main()
