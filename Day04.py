cb63195fad3d454946d0f7a58e4d5bc770fe4d16
Simon Biickert <simon@biickert.ca>
2020-12-30 14:47:29 -0700

    Day 4 part 2 complete

--- Day04.py
+++ Day04.py
@@ -5,17 +5,32 @@ RANGE_HIGH = 820401
 
 def meets_criteria(num: int) -> bool:
 	str_num = str(num)
-	found_adj_same = False
+	possible_adjacents = []
 	digits_decrease = True
 		
 	for i in range(0, 5):
-		if found_adj_same or str_num[i] == str_num[i+1]:
-			found_adj_same = True
-		if str_num[i] > str_num[i+1]:
+		this_num = str_num[i]
+		next_num = str_num[i+1]
+		
+		if this_num == next_num and this_num not in possible_adjacents:
+			possible_adjacents.append(this_num)
+			
+		if this_num > next_num:
 			# Can compare strings b/c '0' is less than '1'
 			digits_decrease = False
 			break
-		
+	
+	found_adj_same = False
+	for poss in possible_adjacents:
+		# We found 2, make sure not 3 in a row
+		if ((3 * poss) in str_num) == True:
+			#print(f'Found adjacent {poss} in {str_num}')
+			#print('but more than 2 in a row.')
+			pass
+		else:
+			found_adj_same = True
+			break
+	
 	return digits_decrease and found_adj_same
 
 def main():
