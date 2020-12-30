9305a36dec1f2056680071334ea16bdcd8ca761d
Simon Biickert <simon@biickert.ca>
2020-12-30 10:23:58 -0700

    Completed Day 3 part 2.

--- Day03.py
+++ Day03.py
@@ -1,6 +1,6 @@
 # Day 3: Crossed Wires
 
-from typing import List, Set
+from typing import List, Dict, Set
 import AoC
 
 def delta_for_dir(dir: str) -> List[int]:
@@ -28,6 +28,26 @@ def bread_crumbs(wire: List[str]) -> Set[str]:
 			loc[1] += delta[1]
 			trail.add(f'{loc[0]},{loc[1]}')
 	return trail
+	
+def calc_signal_distances(wire: List[str], cross_keys: List[str]) -> Dict[str, int]:
+	d = {}
+	loc = [0,0]
+	distance = 0
+	
+	for run in wire:
+		dir = run[0]
+		length = int(run[1:])
+		delta = delta_for_dir(dir)
+		
+		for i in range(0, length):
+			loc[0] += delta[0]
+			loc[1] += delta[1]
+			distance += 1
+			key = f'{loc[0]},{loc[1]}'
+			if key in cross_keys:
+				d[key] = distance
+	return d
+
 
 def manhattan_dist(x: int, y: int) -> int:
 	return abs(x) + abs(y)
@@ -47,6 +67,16 @@ def main():
 	distances = sorted(list(map(lambda coord: (manhattan_dist(coord[0], coord[1])), cross_coords)))
 	#print(distances)
 	print(f"The closest cross is at distance {distances[0]}")
-	
+	signal_distances1 = calc_signal_distances(wire1, crosses)
+	#print(signal_distances1)
+	signal_distances2 = calc_signal_distances(wire2, crosses)
+	sum_sig_dist:List[int] = []
+	for key in signal_distances1:
+		sd1 = signal_distances1[key]
+		sd2 = signal_distances2[key]
+		sum_sig_dist.append(sd1 + sd2)
+	sum_sig_dist.sort()
+	print(f'The closest crossing in signal distance is {sum_sig_dist[0]}')
+				
 if __name__ == '__main__':
 	main()
