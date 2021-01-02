cd12b80821ca3b6a5560447d1df77d4f44c6ee15
Simon Biickert <simon@biickert.ca>
2021-01-01 17:54:49 -0700

    Day 5 part 2 complete

--- Day05.py
+++ Day05.py
@@ -22,35 +22,84 @@ def get_program_value(program: List[int], value: int, mode: int) -> int:
 	# Position mode
 	return program[value]
 
+def get_program_values(program: List[int], ptr: int, count: int, modes: Tuple[int, int, int]) -> List[int]:
+	values = []
+	for offset in range(1, count + 1):
+		value = get_program_value(program, program[ptr+offset], modes[offset-1])
+		values.append(value)
+	return values
+
 def cycle_computer(ptr: int, program: List[int], input: int) -> Tuple[int, int, int]:
 	# Read instruction at ptr
 	output = 0
 	in_ptr = ptr # holding for printing later
 	instr = parse_instruction(program[ptr])
 	opcode = instr[0]
-	if opcode == 1 or opcode == 2:
-		# Add (1) or Multiply (2)
-		value1 = get_program_value(program, program[ptr+1], instr[1])
-		value2 = get_program_value(program, program[ptr+2], instr[2])
+	modes = instr[1:]
+	if opcode == 1:
+		# Add
+		values = get_program_values(program, ptr, 2, modes)
 		writeAddress = program[ptr+3]
-		#print(f'[{read1Address}], [{read2Address}] -> [{writeAddress}]')
-		if opcode == 1:
-			action = f'write {value1} + {value2} to {writeAddress}'
-			program[writeAddress] = value1 + value2
-		else:
-			action = f'write {value1} * {value2} to {writeAddress}'
-			program[writeAddress] = value1 * value2
+		action = f'write {values[0]} + {values[1]} to {writeAddress}'
+		program[writeAddress] = values[0] + values[1]
+		ptr += 4
+	elif opcode == 2:
+		# Multiply
+		values = get_program_values(program, ptr, 2, modes)
+		writeAddress = program[ptr+3]
+		action = f'write {values[0]} + {values[1]} to {writeAddress}'
+		program[writeAddress] = values[0] * values[1]
 		ptr += 4
 	elif opcode == 3:
+		# Write input
 		writeAddress = program[ptr+1]
 		action = f'write {input} to address {writeAddress}'
 		program[writeAddress] = input
 		ptr += 2
 	elif opcode == 4:
-		readAddress = program[ptr+1]
-		output = program[readAddress]
+		# Read and send to output
+		readAddress = ptr+1
+		output = get_program_value(program, program[readAddress], modes[0])
 		action = f'read {output} from address {readAddress}'
 		ptr += 2
+	elif opcode == 5:
+		# Jump if true
+		values = get_program_values(program, ptr, 2, modes)
+		if values[0] > 0:
+			action = f'jump {ptr} to address {values[1]}'
+			ptr = values[1]
+		else:
+			action = 'jump if true no action'
+			ptr += 3
+	elif opcode == 6:
+		# Jump if false
+		values = get_program_values(program, ptr, 2, modes)
+		if values[0] == 0:
+			action = f'jump {ptr} to address {values[1]}'
+			ptr = values[1]
+		else:
+			action = 'jump if false no action'
+			ptr += 3
+	elif opcode == 7:
+		# Less than
+		values = get_program_values(program, ptr, 2, modes)
+		writeAddress = program[ptr+3]
+		if values[0] < values[1]:
+			program[writeAddress] = 1
+		else:
+			program[writeAddress] = 0
+		action = f'write {program[writeAddress]} to address {writeAddress}'
+		ptr += 4
+	elif opcode == 8:
+		# Equals
+		values = get_program_values(program, ptr, 2, modes)
+		writeAddress = program[ptr+3]
+		if values[0] == values[1]:
+			program[writeAddress] = 1
+		else:
+			program[writeAddress] = 0
+		action = f'write {program[writeAddress]} to address {writeAddress}'
+		ptr += 4
 	elif opcode == 99:
 		# End program
 		action = 'end program'
@@ -60,17 +109,27 @@ def cycle_computer(ptr: int, program: List[int], input: int) -> Tuple[int, int, 
 		ptr += 1
 		action = f"Unknown opcode {opcode}"
 		print(action)
-	print(f'{in_ptr} -> {instr} -> {action}')
+	#print(f'{in_ptr} -> {instr} -> {action}')
 	return (ptr, opcode, output)
 
 def main():
 		program_str = AoC.read_input_file("Day05_Input.txt", True)
-		program = list(map(lambda x: (int(x)), program_str[2].split(',')))
-		input = 1 # the ID for the ship's air conditioner unit
+		program = list(map(lambda x: (int(x)), program_str[3].split(',')))
+		
+		#input = 1 # the ID for the ship's air conditioner unit
+		input = 5 # the ID for the ship's thermal radiator controller
+		print(f'Input: {input}')
+		
 		ptr = 0
 		opcode = 0
+		output = 0
+		#print(program)
+		
 		while opcode != 99:
-			(ptr, opcode, output) = cycle_computer(ptr, program, input)
+			(ptr, opcode, cycle_output) = cycle_computer(ptr, program, input)
+			if cycle_output != 0:
+				output = cycle_output
+				
 		print(f'Output: {output}')
 
 if __name__ == "__main__":
