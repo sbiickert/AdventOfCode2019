e80f23b2abb22f1a4bf006277befba5b6328c00b
Simon Biickert <simon@biickert.ca>
2022-01-03 13:20:09 -0700

    Day 7 part 2 complete
    First new star! Woot!

--- AdventOfCode2019/AdventOfCode2019/Solutions/Day07_AmplificationCircuit.swift
+++ AdventOfCode2019/AdventOfCode2019/Solutions/Day07_AmplificationCircuit.swift
@@ -13,33 +13,47 @@ struct AmplificationCircuit: AoCSolution {
 		print("\nDay 07 (Amplification Circuit) -> \(filename)")
 		let input = AoCUtil.readInputFile(named: filename, removingEmptyLines: true)
 		let program = input[0].split(separator: ",").map({Int(String($0))!})
-		let ampSettings = AoCUtil.cRangeToArray(r: 0...4)
+		let pt1Settings = AoCUtil.cRangeToArray(r: 0...4)
 		
 		var maxOutput = 0
-		for perm in ampSettings.permutations(ofCount: 5) {
+		for perm in pt1Settings.permutations(ofCount: 5) {
 			let output = calcOutput(program, phaseSettings: perm)
 			if output > maxOutput {
 				maxOutput = output
-				print("Phase settings \(perm) produced \(output)")
+				//print("Phase settings \(perm) produced \(output)")
 			}
 		}
 
 		print("Part One")
 		print("The maximum output is: \(maxOutput)")
 
+		let pt2Settings = AoCUtil.cRangeToArray(r: 5...9)
+		maxOutput = 0
+		for perm in pt2Settings.permutations(ofCount: 5) {
+			let output = calcFeedbackOutput(program, phaseSettings: perm)
+			if output > maxOutput {
+				maxOutput = output
+				//print("Phase settings \(perm) produced \(output)")
+			}
+		}
+
+		print("Part Two")
+		print("The maximum output is: \(maxOutput)")
+
 	}
 	
 	static func runTests(filename: String) {
 		print("\nDay 07 TEST (Amplification Circuit) -> \(filename)")
-		let input = AoCUtil.readInputFile(named: filename, removingEmptyLines: true)
+		let groupedInput = AoCUtil.readGroupedInputFile(named: filename)
+		let pt1Input = groupedInput[0]
 		
-		let ampSettings = AoCUtil.cRangeToArray(r: 0...4)
+		let pt1AmpSettings = AoCUtil.cRangeToArray(r: 0...4)
 
-		for line in input {
+		for line in pt1Input {
 			print("program: \(line)")
-			var program = line.split(separator: ",").map({Int(String($0))!})
+			let program = line.split(separator: ",").map({Int(String($0))!})
 			var maxOutput = 0
-			for perm in ampSettings.permutations(ofCount: 5) {
+			for perm in pt1AmpSettings.permutations(ofCount: 5) {
 				let output = calcOutput(program, phaseSettings: perm)
 				if output > maxOutput {
 					maxOutput = output
@@ -48,6 +62,43 @@ struct AmplificationCircuit: AoCSolution {
 			}
 		}
 		
+		let pt2Input = groupedInput[1]
+		let pt2AmpSettings = AoCUtil.cRangeToArray(r: 5...9)
+
+		for line in pt2Input {
+			print("program: \(line)")
+			let program = line.split(separator: ",").map({Int(String($0))!})
+			var maxOutput = 0
+			for perm in pt2AmpSettings.permutations(ofCount: 5) {
+				let output = calcFeedbackOutput(program, phaseSettings: perm)
+				if output > maxOutput {
+					maxOutput = output
+					print("Phase settings \(perm) produced \(output)")
+				}
+			}
+		}
+
+	}
+	
+	static private func calcFeedbackOutput(_ program: [Int], phaseSettings: [Int]) -> Int {
+		var computers = [IntCodeComputer]()
+		for i in 0..<5 {
+			computers.append(IntCodeComputer(program))
+			computers[i].input.append(phaseSettings[i])
+		}
+		
+		var previousOutput = 0
+		while computers[0].isHalted == false {
+			for i in 0..<5 {
+				computers[i].input.append(previousOutput)
+				computers[i].run(toOutput: true, resetPtr: false)
+				if computers[i].isHalted {
+					break
+				}
+				previousOutput = computers[i].output[0]
+			}
+		}
+		return previousOutput
 	}
 	
 	static private func calcOutput(_ program: [Int], phaseSettings: [Int]) -> Int {
@@ -57,22 +108,29 @@ struct AmplificationCircuit: AoCSolution {
 			computer.program = program
 			computer.input.append(phaseSettings[amp])
 			computer.input.append(previousOutput)
-			computer.run()
+			computer.run(toOutput: false, resetPtr: true)
 			previousOutput = computer.output[0]
 		}
 		return previousOutput
 	}
-	
+
 	class IntCodeComputer {
 		var program = [Int]()
 		
 		var input = [Int]()
 		var output = [Int]()
+		var ptr = 0
+		var isHalted = false
 		
 		init() {}
 		
-		func run() {
-			var ptr = 0
+		init(_ program: [Int]) {
+			self.program = program
+		}
+
+		func run(toOutput: Bool, resetPtr: Bool) {
+			if resetPtr { ptr = 0 }
+			isHalted = false
 			output.removeAll()
 			
 			var bExit = false
@@ -83,6 +141,7 @@ struct AmplificationCircuit: AoCSolution {
 				switch instr.op {
 				case .exit:
 					bExit = true
+					isHalted = true
 				case .add:
 					let p = readParams(count: 2)
 					let result = p[1] + p[2]
@@ -104,6 +163,9 @@ struct AmplificationCircuit: AoCSolution {
 					let p = readParams(count: 1)
 					output.append(p[1])
 					ptr += instr.op.size
+					if toOutput {
+						bExit = true
+					}
 				case .jmpT:
 					let p = readParams(count: 2)
 					if p[1] != 0 {
