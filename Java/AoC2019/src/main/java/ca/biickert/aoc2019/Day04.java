c62a480ed65809bf7cc9e11b8ad1e448832d2eda
Simon Biickert <simon@biickert.ca>
2023-01-02 06:08:39 -0700

    Day 4 part 2 complete in Java

--- Java/AoC2019/src/main/java/ca/biickert/aoc2019/Day04.java
+++ Java/AoC2019/src/main/java/ca/biickert/aoc2019/Day04.java
@@ -11,7 +11,7 @@ import java.util.List;
  * @author sjb
  */
 public class Day04 extends Solution {
-    
+
     static final int INPUT_MIN = 359282;
     static final int INPUT_MAX = 820401;
 
@@ -33,30 +33,60 @@ public class Day04 extends Solution {
 
     private int solvePartOne() {
 	int validCount = 0;
-	
+
 	for (int i = INPUT_MIN; i <= INPUT_MAX; i++) {
 	    List<String> chars = Arrays.asList(String.valueOf(i).split(""));
 	    List<Integer> digits = chars.stream().map(Integer::valueOf).toList();
-	    boolean hasAdjacentPair = false;
-	    boolean doesNeverDecrease = true;
-	    for (int j = 1; j < digits.size(); j++) {
-		if (digits.get(j-1).equals(digits.get(j))) {
-		    hasAdjacentPair = true;
-		}
-		if (digits.get(j-1).intValue() > digits.get(j)) {
-		    doesNeverDecrease = false;
-		    break;
-		}
-	    }
-	    if (hasAdjacentPair && doesNeverDecrease) {
+	    if (hasAdjacent(digits) && doesNeverDecrease(digits)) {
 		validCount++;
 	    }
 	}
-	
+
 	return validCount;
     }
 
     private int solvePartTwo() {
-	return 0;
+	int validCount = 0;
+
+	for (int i = INPUT_MIN; i <= INPUT_MAX; i++) {
+	    List<String> chars = Arrays.asList(String.valueOf(i).split(""));
+	    List<Integer> digits = chars.stream().map(Integer::valueOf).toList();
+	    if (hasAdjacentPair(digits) && doesNeverDecrease(digits)) {
+		validCount++;
+	    }
+	}
+
+	return validCount;
+    }
+
+    private boolean hasAdjacent(List<Integer> digits) {
+	for (int j = 1; j < digits.size(); j++) {
+	    if (digits.get(j - 1).equals(digits.get(j))) {
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    private boolean hasAdjacentPair(List<Integer> digits) {
+	for (int i = -1; i <= digits.size()-3; i++) {
+	    int a = (i < 0) ? -1 : digits.get(i);
+	    int b = digits.get(i+1);
+	    int c = digits.get(i+2);
+	    int d = (i + 4 > digits.size()) ? -1 : digits.get(i+3);
+	    if (a != b && b == c && c != d) {
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    private boolean doesNeverDecrease(List<Integer> digits) {
+	for (int j = 1; j < digits.size(); j++) {
+	    if (digits.get(j - 1) > digits.get(j)) {
+		return false;
+	    }
+	}
+	return true;
     }
 }
