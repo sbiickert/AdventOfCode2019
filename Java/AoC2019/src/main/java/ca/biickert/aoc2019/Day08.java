be6bf6470f9e6fd5efed5225dd7401eb32c9545e
Simon Biickert <simon@biickert.ca>
2023-01-04 08:18:24 -0700

    Day 8 part 2 complete in Java

--- Java/AoC2019/src/main/java/ca/biickert/aoc2019/Day08.java
+++ Java/AoC2019/src/main/java/ca/biickert/aoc2019/Day08.java
@@ -24,9 +24,9 @@ public class Day08 extends Solution {
 	List<String> input = InputReader.readGroupedInputFile(filename, index);
 
 	SpaceImage image = new SpaceImage(input.get(0));
-	
+
 	var part1Solution = solvePartOne(image);
-	var part2Solution = solvePartTwo();
+	var part2Solution = solvePartTwo(image);
 
 	result = new Result(Integer.toString(part1Solution), Integer.toString(part2Solution));
 
@@ -37,17 +37,23 @@ public class Day08 extends Solution {
 	int layerIndex = 0;
 	int least = Integer.MAX_VALUE;
 	int result = 0;
-	
+
 	for (int l = 0; l < image.getLayerCount(); l++) {
 	    int zeroCount = 0;
 	    int oneCount = 0;
 	    int twoCount = 0;
 	    for (int r = 0; r < image.getHeight(); r++) {
 		for (int c = 0; c < image.getWidth(); c++) {
-		    int digit = image.getDigit(l,r,c);
-		    if (digit == 0) { zeroCount++; }
-		    if (digit == 1) { oneCount++; }
-		    if (digit == 2) { twoCount++; }
+		    int digit = image.getDigit(l, r, c);
+		    if (digit == 0) {
+			zeroCount++;
+		    }
+		    if (digit == 1) {
+			oneCount++;
+		    }
+		    if (digit == 2) {
+			twoCount++;
+		    }
 		}
 	    }
 	    if (zeroCount < least) {
@@ -56,24 +62,26 @@ public class Day08 extends Solution {
 		result = oneCount * twoCount;
 	    }
 	}
-	
+
 	return result;
     }
 
-    private int solvePartTwo() {
+    private int solvePartTwo(SpaceImage image) {
+	image.print();
 	return 0;
     }
 
 }
 
 class SpaceImage {
+
     private int width;
     private int height;
     private List<Integer> digits;
-    
+
     public SpaceImage(String defn) {
-        var list = Arrays.asList(defn.split(""));
-        digits = list.stream().map(Integer::valueOf).toList();
+	var list = Arrays.asList(defn.split(""));
+	digits = list.stream().map(Integer::valueOf).toList();
 	switch (digits.size()) {
 	    case 12:
 		width = 3;
@@ -96,13 +104,37 @@ class SpaceImage {
     public int getHeight() {
 	return height;
     }
-    
+
     public int getLayerCount() {
 	return digits.size() / (width * height);
     }
-    
+
     public int getDigit(int layer, int row, int col) {
 	int index = (layer * width * height) + (row * width) + col;
 	return digits.get(index);
+    }
+
+    static final String WHITE = "#";
+    static final String BLACK = " ";
+
+    public void print() {
+	for (int r = 0; r < height; r++) {
+	    String line = "";
+	    for (int c = 0; c < width; c++) {
+		String color = "";
+		for (int l = getLayerCount() - 1; l >= 0; l--) {
+		    switch (getDigit(l, r, c)) {
+			case 0:
+			    color = BLACK;
+			    break;
+			case 1:
+			    color = WHITE;
+			    break;
+		    }
+		}
+		line += color;
+	    }
+	    System.out.println(line);
+	}
     }
 }
