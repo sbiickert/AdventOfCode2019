c3b16f2edf1cc1ed3b82e1ea85ceeecf235d4dc4
Simon Biickert <simon@biickert.ca>
2023-01-06 10:39:18 -0700

    Day 14 part 2 improved
    Projecting forward, waiting for the averages to level out.

--- Java/AoC2019/src/main/java/ca/biickert/aoc2019/Day14.java
+++ Java/AoC2019/src/main/java/ca/biickert/aoc2019/Day14.java
@@ -4,10 +4,10 @@ import ca.biickert.aoc2019.util.InputReader;
 import ca.biickert.aoc2019.util.Result;
 import ca.biickert.aoc2019.util.Solution;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  *
@@ -36,38 +36,57 @@ public class Day14 extends Solution {
 
     private String solvePartOne(Map<String, ChemicalFormula> recipes) {
 	stockpile = new HashMap<>();
-	stockpile.put("ORE", 1000000000000L);
+	tracking = new HashMap<>();
 
-	ChemicalQuantity fuel = new ChemicalQuantity("FUEL", 1);
-	generateChemical(fuel, recipes);
+	generateChemical("FUEL", 1, recipes);
 
-	return String.valueOf(1000000000000L - stockpile.get("ORE"));
+	return String.valueOf(tracking.get("ORE"));
     }
 
     private String solvePartTwo(Map<String, ChemicalFormula> recipes) {
 	stockpile = new HashMap<>();
-	stockpile.put("ORE", 1000000000000L);
+	tracking = new TreeMap<>();
 
-	ChemicalQuantity fuel = new ChemicalQuantity("FUEL", 1);
+	int[] trend = new int[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 	int fuelGenerated = 0;
-	while (generateChemical(fuel, recipes)) {
-	    fuelGenerated++;
+	while (generateChemical("FUEL", 1, recipes)) {
+	    fuelGenerated += stockpile.get("FUEL");
 	    stockpile.put("FUEL", 0L);
 	    if (fuelGenerated % 1000 == 0) {
 		System.out.println(fuelGenerated);
+		for (int i = trend.length-1; i > 0; i--) {
+		    trend[i] = trend[i-1];
+		}
+		var avg = (tracking.get("ORE") / (double)fuelGenerated);
+		trend[0] = (int)Math.round(1000000000000L / avg);
+		//System.out.println("ORE per FUEL: " + avg);
+		System.out.println("Projected FUEL from 1 Trillion ORE: " + trend[0]);
+		int largest = 0;
+		for (int i = 0; i < trend.length; i++) {
+		    largest = Math.max(largest, trend[i]);
+		}
+		// Looking for a run of 10 identical projections
+		boolean agreement = true;
+		for (int i = 1; i < 10; i++) {
+		    agreement = agreement && trend[i] == trend[0];
+		}
+		if (agreement) {
+		    return String.valueOf(largest);
+		}
 	    }
 	}
 
-	return String.valueOf(fuelGenerated);
+	return "Did not find trend.";
     }
 
     private Map<String, Long> stockpile;
+    private Map<String, Long> tracking;
 
     private long getStockpileLevel(String chemical) {
-	if (stockpile.containsKey(chemical) == false) {
-	    return 0;
+	if (stockpile.containsKey(chemical)) {
+	    return stockpile.get(chemical);
 	}
-	return stockpile.get(chemical);
+	return 0;
     }
 
     private void addToStockpile(String chemical, int amount) {
@@ -75,6 +94,14 @@ public class Day14 extends Solution {
 	long newLevel = oldLevel + amount;
 	//System.out.println(String.format("ADD %d %s to %d -> %d", amount, chemical, oldLevel, newLevel));
 	stockpile.put(chemical, newLevel);
+	tracking.put(chemical, getTrackingLevel(chemical) + amount);
+    }
+
+    private long getTrackingLevel(String chemical) {
+	if (tracking.containsKey(chemical)) {
+	    return tracking.get(chemical);
+	}
+	return 0;
     }
 
     private void takeFromStockpile(String chemical, int amount) {
@@ -88,28 +115,30 @@ public class Day14 extends Solution {
 	}
     }
 
-    private boolean generateChemical(ChemicalQuantity cq,
+    private boolean generateChemical(String chemical, int amount,
 	    Map<String, ChemicalFormula> recipes) {
 	//System.out.println("Generating " + cq.amount() + " " + cq.chemical());
-	ChemicalFormula cf = recipes.get(cq.chemical());
+	ChemicalFormula cf = recipes.get(chemical);
 	boolean success = true;
-	while (getStockpileLevel(cq.chemical()) < cq.amount()) {
+	while (getStockpileLevel(chemical) < amount) {
 	    for (var input : cf.inputs()) {
 		long needed = (long) input.amount() - getStockpileLevel(input.chemical());
 		//System.out.println("Need " + needed + " " + input.chemical());
 		if (needed > 0) {
 		    if (input.chemical().equals("ORE")) {
-			// We're out of ORE!
-			return false;
+			addToStockpile("ORE", (int)needed);
+			// Are we out of ORE?
+			success = success && (getTrackingLevel("ORE") < 1000000000000L);
+		    } else {
+			success = success && generateChemical(input.chemical(), input.amount(), recipes);
 		    }
-		    success = success && generateChemical(new ChemicalQuantity(input.chemical(), input.amount()), recipes);
 		    if (!success) {
 			return success;
 		    }
 		}
 		takeFromStockpile(input.chemical(), input.amount());
 	    }
-	    addToStockpile(cq.chemical(), cf.product().amount());
+	    addToStockpile(chemical, cf.product().amount());
 	}
 	return success;
     }
