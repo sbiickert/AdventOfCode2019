d294557f79c14eebd01ecd7a2fa77bd6b8105293
Simon Biickert <simon@biickert.ca>
2023-01-16 14:05:24 -0700

    Day 19 part 2 complete in Java

--- Java/AoC2019/src/main/java/ca/biickert/aoc2019/Day19.java
+++ Java/AoC2019/src/main/java/ca/biickert/aoc2019/Day19.java
@@ -15,6 +15,8 @@ import java.util.List;
  */
 public class Day19 extends Solution {
 
+    IntCodeComputer computer;
+
     public Day19() {
 	super(19, "Tractor Beam", true);
     }
@@ -25,7 +27,9 @@ public class Day19 extends Solution {
 
 	List<String> input = InputReader.readGroupedInputFile(filename, index);
 
-	var part1Solution = solvePartOne(input.get(0));
+	computer = new IntCodeComputer(input.get(0));
+
+	var part1Solution = solvePartOne();
 	var part2Solution = solvePartTwo();
 
 	result = new Result(part1Solution, part2Solution);
@@ -33,30 +37,74 @@ public class Day19 extends Solution {
 	return result;
     }
 
-    private String solvePartOne(String program) {
-	var computer = new IntCodeComputer(program);
+    private String solvePartOne() {
 	var monitor = new Grid2D(".", AdjacencyRule.QUEEN);
-	
+
 	for (int x = 0; x < 50; x++) {
 	    for (int y = 0; y < 50; y++) {
-		var state = computer.saveState();
-		computer.input.add((long)x);
-		computer.input.add((long)y);
-		computer.run(true, false); // Not resetting the pointer
-		var output = computer.output;
-		monitor.set(new Coord2D(x, y), output == 1L ? "#" : ".");
-		computer.restoreState(state);
+		if (isInTractorBeam(x, y)) {
+		    monitor.set(new Coord2D(x, y), "#");
+		}
 	    }
 	}
-	
+
 	var coords = monitor.getCoordsWithValue("#");
-	
-	monitor.print();
+
+	//monitor.print();
 	return String.valueOf(coords.size());
     }
 
     private String solvePartTwo() {
-	return "";
+	// 0,0 is at the ship.
+	// Worked out the beam bounds at y = 100 to find slopes of the sides
+	// But then realized it would be easier just to brute-force
+	// Start at y = 1000
+	int x = 0;
+	int y = 1000;
+	boolean bFoundBeam = false;
+	int minX = 0;
+	var monitor = new Grid2D(".", AdjacencyRule.ROOK);
+	int result = -1;
+
+	while (true) {
+	    boolean inBeam = isInTractorBeam(x, y);
+	    if (inBeam) {
+		monitor.set(new Coord2D(x, y), "#");
+	    }
+	    if (!bFoundBeam && inBeam) {
+		minX = x;
+		var c = new Coord2D(x + 99, y - 99);
+		if (monitor.get(c).equals("#")) {
+		    // We have found a 100x100 space.
+		    result = 10000 * x + (y - 99);
+		    break;
+		}
+	    }
+	    bFoundBeam = bFoundBeam || inBeam;
+	    if (bFoundBeam && !inBeam) {
+		bFoundBeam = false;
+		if (y % 100 == 0) {
+		    System.out.println(y);
+		}
+		y++;
+		x = minX;
+	    } else {
+		x++;
+	    }
+	}
+
+	//monitor.print();
+
+	return String.valueOf(result);
     }
 
+    private boolean isInTractorBeam(int x, int y) {
+	var state = computer.saveState();
+	computer.input.add((long) x);
+	computer.input.add((long) y);
+	computer.run(true, false); // Not resetting the pointer
+	var output = computer.output;
+	computer.restoreState(state);
+	return output == 1L;
+    }
 }
